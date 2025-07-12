b435e5786431c22bb19b63f5f6f165ddfe78e217
Pritom-Majumder <pritommajumder12345@gmail.com>
2025-07-12 20:34:14 +0600

    Set_Fix_Pin & link_up_another_html(page)

--- code/script/login.js
+++ code/script/login.js
@@ -7,23 +7,20 @@ document.getElementById("login-btn").addEventListener("click",
 
     const accountNumber = document.getElementById("Account-number").value;
    const Pin = document.getElementById("pin").value;
-   const convertedPin = parseInt(Pin);
+   const convertedPin = parseInt(Pin); //converted_from_string_to_number"
 
    if(accountNumber.length===11){
     //   console.log("yes, valid Account Number");
       if(convertedPin===1234){
-        console.log("Okay, pin is good");
+        window.location.href="./main.html"
       }
       else{
         console.log("Pin incorrect");
-        
       }
    }
    else{
     console.log("Need Valid Account Number");
     
    }
-
-   
     
 })
