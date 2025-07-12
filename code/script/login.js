1e67ef3c17592d49659913a7e3225b4ebc3bfef0
Pritom-Majumder <pritommajumder12345@gmail.com>
2025-07-12 19:31:12 +0600

    no_again_reload & get_Id to catch

--- code/script/login.js
+++ code/script/login.js
@@ -3,7 +3,11 @@
 
 document.getElementById("login-btn").addEventListener("click", 
     function(event){
-    event.preventDefault();
-    
+    event.preventDefault(); //no again reload
+
+    const accountNumber = document.getElementById("Account-number").value;
+   const Pin = document.getElementById("pin").value;
+   console.log(accountNumber, Pin);
+   
     
 })
