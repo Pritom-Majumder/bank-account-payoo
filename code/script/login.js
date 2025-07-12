79caec3d5ea8ceb10b209330a44b81ef08331862
Pritom-Majumder <pritommajumder12345@gmail.com>
2025-07-12 21:59:12 +0600

    separate_index & main.html

--- code/script/login.js
+++ code/script/login.js
@@ -9,17 +9,18 @@ document.getElementById("login-btn").addEventListener("click",
    const Pin = document.getElementById("pin").value;
    const convertedPin = parseInt(Pin); //converted_from_string_to_number"
 
-   if(accountNumber.length===11){
+   if(accountNumber.length === 11){
     //   console.log("yes, valid Account Number");
-      if(convertedPin===1234){
+      if(convertedPin === 1234){
+        // window.location.href="./main.html";//link up the another html file
         window.location.href="./main.html"
       }
       else{
-        console.log("Pin incorrect");
+       alert("Pin incorrect");
       }
    }
    else{
-    console.log("Need Valid Account Number");
+    alert("Need Valid Account Number");
     
    }
     
