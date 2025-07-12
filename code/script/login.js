640ffb64b2f0f2434cc14827c7f983a6f8f23ba7
Pritom-Majumder <pritommajumder12345@gmail.com>
2025-07-12 19:57:18 +0600

    added_condition of Pin & converted_from_string_to_number

--- code/script/login.js
+++ code/script/login.js
@@ -7,9 +7,17 @@ document.getElementById("login-btn").addEventListener("click",
 
     const accountNumber = document.getElementById("Account-number").value;
    const Pin = document.getElementById("pin").value;
+   const convertedPin = parseInt(Pin);
+
    if(accountNumber.length===11){
-      console.log("yes, valid Account Number");
-      
+    //   console.log("yes, valid Account Number");
+      if(convertedPin===1234){
+        console.log("Okay, pin is good");
+      }
+      else{
+        console.log("Pin incorrect");
+        
+      }
    }
    else{
     console.log("Need Valid Account Number");
