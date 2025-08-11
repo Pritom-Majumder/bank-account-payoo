878450810573ce5160b8d7e97d97dedf5014f3a9
Pritom-Majumder <pritommajumder12345@gmail.com>
2025-08-11 14:22:40 +0600

    [not_included]test_click_vanish

---
+++ test/test.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-ban').addEventListener("click", function(){
+    document.getElementById('bd').style.display = "block";
+    document.getElementById('uk').style.display = "none";
+})
+
+document.getElementById('btn-uk').addEventListener("click", function(){
+     document.getElementById('uk').style.display = "block";
+    document.getElementById('bd').style.display = "none";
+})
