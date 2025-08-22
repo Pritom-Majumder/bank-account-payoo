65f4f525d4b88ff806bff22ec78ea42196eee12f
Pritom-Majumder <pritommajumder12345@gmail.com>
2025-08-23 01:40:41 +0600

    added_addMoney_section

---
+++ code/script/toggle.js
@@ -0,0 +1,14 @@
+document.getElementById("addmoney").style.display ="block"; // by default firstly one thing is show
+// document.getElementById("cashOut").style.display = "none"; // by default firstly another thing isn't show
+
+
+
+document.getElementById('add-money-box').addEventListener("click", function(){
+    document.getElementById("addmoney").style.display ="block";
+    document.getElementById("cashOut").style.display = "none";
+})
+
+document.getElementById('cash-out-box').addEventListener("click", function(){
+    document.getElementById("cashOut").style.display ="block";
+    document.getElementById("addmoney").style.display = "none";
+})
